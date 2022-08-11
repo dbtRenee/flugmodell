e917008c442f81139a701b30d6f56322b23e0032
dbtRenee <renee.zunker@stud.hs-hannover.de>
2022-08-11 19:25:37 +0200

    okayyyyyy

--- models/example/Links/origin_link.sql
+++ models/example/Links/origin_link.sql
@@ -1,1 +1,8 @@
-{{ dbtvault.link(var('src_pk'), var('src_fk'), var('source_model'))        }}
+select 
+    AP.ID as AIRPORT_ID,
+    CA.ID as flight_ID,
+    'stg_airport, stg_carrier' as Record_source,
+  {{dbt_utils.current_timestamp()}} as LOAD_DATE
+   from airport_hub as AP, flight_hub as CA
+
+
